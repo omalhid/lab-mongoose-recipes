1e2feab82e1f4d57bcfec34f25f198179afcccc0
Omayma El Hadari <om.elhadari@gmail.com>
2023-12-04 20:58:13 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -17,7 +17,53 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Rfissa',
+      level: 'UltraPro Chef',
+      ingredients:["1 large chicken, - quartered or left whole",
+        "3 large onions, - thinly sliced",
+        "75 ml olive oil",
+        "1 tbsp salt",
+        "1 tbsp pepper",
+        "1 1/2 tbsp ginger",
+        "1 tbsp Ras el Hanout - (or 1 1/2 tbsp msakhen)",
+        "1 1/2 tsp turmeric",
+        "100 g uncooked lentils, green or brown",
+        "4 tbsp fenugreek seeds, - soaked overnight and drained",
+        "1 1/2 tsp saffron threads, - heated gently and then crumbled",
+        "1 handful fresh cilantro, - finely chopped",
+        "1 handful fresh parsley, - finely chopped",
+        "1.15 l water",
+        "1 1/2 tsp smen - (Moroccan preserved butter)",
+        "1 to 1 1/2 batches msemen, shredded - (or trid pastry)"],
+      cuisine:'Moroccan',
+      dishType:'main_course',
+      image: 'https://tasteofmaroc.com/wp-content/uploads/2017/11/rfissa-2b.jpg.webp',
+      duration: 120,
+      creator: 'OmElHid',
+    })
   })
+
+  .then((newRecipe)=>{
+    console.log(newRecipe.title);
+    return Recipe.insertMany(data)
+  })
+
+  .then((manyRecipes)=>{
+    manyRecipes.forEach(recipe => console.log(recipe.title))
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  })
+
+  .then(()=>{
+    console.log('Recipe updated with success');
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+
+  .then (()=>{
+    console.log('Recipe deleted with success');
+    mongoose.disconnect()
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
